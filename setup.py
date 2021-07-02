01ad890b2c78e9b57b1c1227e79cd51f436f0068
Crankrune <40170287+Crankrune@users.noreply.github.com>
2021-07-01 21:44:59 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ext_pathlib",
+    description="Extended Pathlib module.",
+    version="2.0",
+    py_modules=["ext_pathlib"],
+    install_requires=[],
+    include_package_data=True,
+    zip_safe=False,
+)
